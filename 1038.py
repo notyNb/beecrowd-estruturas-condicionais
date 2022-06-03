cebf3ec5a01527a3d55bc7793ba926d43a3786b5
Pedro Lucas <100409572+notyNb@users.noreply.github.com>
2022-06-03 00:25:42 -0400

    Create 1038.py

---
+++ 1038.py
@@ -0,0 +1,28 @@
+a, b = map(int,input().split())
+
+c1 = float(4.00)
+
+c2 = float(4.50)
+
+c3 = float(5.00)
+
+c4 = float(2.00)
+
+c5 = float(1.50)
+
+
+
+if a == 1:
+    print(f"Total: R$ {c1*b:.2f}")
+
+elif a == 2:
+    print(f"Total: R$ {c2*b:.2f}")
+
+elif a == 3:
+    print(f"Total: R$ {c3*b:.2f}")
+
+elif a == 4:
+    print(f"Total: R$ {c4*b:.2f}")
+
+elif a == 5:
+    print(f"Total: R$ {c5*b:.2f}")
